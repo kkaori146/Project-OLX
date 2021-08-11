b718601647072e51d8a2b70b4086df6ef3592d9f
Kaminishikawahara KK <kkaori146@gmail.com>
2021-08-11 12:56:06 +0900

    Route created

--- olx/frontend/src/App.js
+++ olx/frontend/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+import Routes from './Routes';
 
 const Page = (props) => {
   return (
-    <div>Opa, funcionando....</div>
+    <BrowserRouter>
+     <Routes/>
+    </BrowserRouter>
+   
   );
 }
 
