f5f955d7e621fdc9022186d247143939b41c32d0
Kaminishikawahara KK <kkaori146@gmail.com>
2021-08-11 13:26:10 +0900

    Initial Structure: Frontend (src)

--- olx/frontend/src/App.js
+++ olx/frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
+import { Template } from './components/MainComponents';
+import Header from './components/partials/Header';
+import Footer from './components/partials/Footer';
 import Routes from './Routes';
 
 const Page = (props) => {
   return (
     <BrowserRouter>
+    <Template>
+     <Header/>
      <Routes/>
+     <Footer/>
+    </Template>
     </BrowserRouter>
    
   );
@@ -17,10 +24,10 @@ const mapStateToProps = (state) => {
     user:state.user
   };
 }
-const mapDisatchToProps = (dispatch) =>{
+const mapDispatchToProps = (dispatch) =>{
   return {
 
   };
 }
 
-export default connect(mapStateToProps, mapDisatchToProps)(Page);
+export default connect(mapStateToProps, mapDispatchToProps)(Page);
