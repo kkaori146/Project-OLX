d25dc0fbe8e5253e97ef7878da83ae481255ca8e
Kaminishikawahara KK <kkaori146@gmail.com>
2021-08-11 13:58:22 +0900

    Header: 1- structure and brand

--- olx/frontend/src/components/partials/Header/index.js
+++ olx/frontend/src/components/partials/Header/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import {HeaderArea} from './styled';
 
 const Header = () => {
     return(
         <HeaderArea>
-            ...
+            <div className="container">
+                <div className="logo">
+<Link to="/">
+<span className="logo-1">K</span>
+<span className="logo-2">Y</span>
+<span className="logo-3">W</span>
+</Link>
+                </div>
+            </div>
         </HeaderArea>
     );
 }
