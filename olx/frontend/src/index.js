976875b8910341b5b0e0493a0ec50fd324637c42
Kaminishikawahara KK <kkaori146@gmail.com>
2021-08-11 11:54:23 +0900

    Redux Configured

--- olx/frontend/src/index.js
+++ olx/frontend/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import App from './App';
+import Reducers from './Reducers';
 
+const store = createStore(Reducers);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+<App />
+</Provider>
+, document.getElementById('root'));
