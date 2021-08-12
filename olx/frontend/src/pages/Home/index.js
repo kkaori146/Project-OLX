2e3fb711e67c364f8aa6d87baacc6e5c8bdcd878
Kaminishikawahara KK <kkaori146@gmail.com>
2021-08-12 17:46:37 +0900

    Home: searchBox

--- olx/frontend/src/pages/Home/index.js
+++ olx/frontend/src/pages/Home/index.js
@@ -1,14 +1,43 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import {SearchArea, PageArea} from './styled';
+import useApi from '../../helpers/OlxAPI';
+import { PageContainer} from '../../components/MainComponents';
+
 
 const Page = () => {
-    return (
-        <div>
-        <h1>Página Inicial</h1>
 
-        <Link to="/about">Sobre</Link>
-        </div>
+    const api = useApi();
+
+   
+    return (
+        <>
+        <SearchArea>
+<PageContainer>
+    <div className="searchBox">
+<form method="GET" action="/ads">
+    <input type="text" name="q" placeholder="Estou procurando...."/>
+    <select name="state">
+        
+        </select> 
+        <button>Procurar</button>
+
+</form>
+    </div>
+    <div className="categoryList">
+
+    </div>
+</PageContainer>
+</SearchArea>
+
+<PageContainer>
+<PageArea>
+    ....
+</PageArea>
+</PageContainer>
+      
+        </>
     );
 }
 
 export default Page;
+        
