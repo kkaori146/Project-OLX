79b8c9801c9062c8ef4cc028652b4ded6e33f381
Kaminishikawahara KK <kkaori146@gmail.com>
2021-08-12 11:34:27 +0900

    Login: Authentication

---
+++ olx/frontend/src/helpers/OlxAPI.js
@@ -0,0 +1,7 @@
+const OlxAPI = {
+login: async (email, password) => {
+    //fazer consulta ao WB
+ return {};
+}
+};
+export default () => OlxAPI; //função que retorna o objeto que no caso é o OlxAPI
