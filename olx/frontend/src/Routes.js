0be0a7226006da3af7704b4dd0ae5e4ca7f6785f
Kaminishikawahara KK <kkaori146@gmail.com>
2021-08-14 10:44:32 +0900

    Ads: Struture 1

--- olx/frontend/src/Routes.js
+++ olx/frontend/src/Routes.js
@@ -8,6 +8,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import AdPage from './pages/AdPage';
 import AddAd from './pages/AddAd';
+import Ads from './pages/Ads';
 
 export default () => {
     return (
@@ -34,6 +35,10 @@ export default () => {
 
             <RouteHandler private exact path="/post-an-ad">
                 <AddAd />
+            </RouteHandler>
+
+            <RouteHandler exact path="/ads">
+                <Ads />
             </RouteHandler>
 
             <RouteHandler>
