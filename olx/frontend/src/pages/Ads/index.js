174dba47dd5c8271a06a81b1d8b0c10bce2ddad3
Kaminishikawahara KK <kkaori146@gmail.com>
2021-08-14 11:20:43 +0900

    Ads: LeftSide (Search Box)

--- olx/frontend/src/pages/Ads/index.js
+++ olx/frontend/src/pages/Ads/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {Link} from 'react-router-dom';
+import {useLocation, useHistory} from 'react-router-dom';
 import {PageArea} from './styled';
 import useApi from '../../helpers/OlxAPI';
 import { PageContainer} from '../../components/MainComponents';
@@ -7,6 +7,16 @@ import AdItem from '../../components/partials/AdItem';
 
 const Page = () => {
     const api = useApi();
+
+    const useQueryString = () => {
+        return new URLSearchParams (useLocation().search);
+    }
+    const query = useQueryString();
+
+    const [q, setQ] = useState( query.get('q') != null ? query.get('q') : '' );
+    const [cat, setCat] = useState( query.get('cat') != null ? query.get('cat') : '' );
+    const [state, setState] = useState( query.get('state') != null ? query.get('state') : '' );
+    
 
     const [stateList, setStateList] = useState([]);
     const [categories, setCategories] = useState([]);
@@ -46,20 +56,25 @@ const Page = () => {
                     <PageArea>
                         <div className="leftSide">
                             <form method="GET">
-                                <input type="text" name="q" />
+                                <input 
+                                    type="text" 
+                                    name="q" 
+                                    placeholder="O que você procura?"
+                                    value={q}
+                                />
 
                                 <div className="filterName">Estado:</div>
-                                <selct name="state">
+                                <select name="state" value={state}>
                                     <option></option>
                                     {stateList.map((i,k)=>
                                         <option key={k} value={i.name}>{i.name}</option>
                                     )}
-                                </selct>
+                                </select>
 
                                 <div className="filterName">Categoria:</div>
                                 <ul>
                                     {categories.map((i,k)=>
-                                        <li key={k} classname="categoryItem">
+                                        <li key={k} classname={cat==i.slug?'categoryItem active':'categoryItem'}>
                                             <img src={i.img} alt="" />
                                             <span>{i.name}</span>
                                         </li>
